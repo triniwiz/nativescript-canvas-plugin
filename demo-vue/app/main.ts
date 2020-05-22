aba88f88b4dbc25abfbadd31ed18d162de46a5fc
Osei Fortune <fortune.osei@yahoo.com>
2020-05-22 06:00:03 -0400

    chore(): update vue demo

--- demo-vue/app/main.ts
+++ demo-vue/app/main.ts
@@ -1,11 +1,11 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
-
+import Canvas from 'nativescript-canvas-plugin/vue';
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
-Vue.registerElement('Canvas', () => require('nativescript-canvas-plugin').TNSCanvas)
+Vue.use(Canvas);
 
 new Vue({
 
